8839e8f5087d896802a69ed3d3178f94e186fa5a
birbalbhutra <bhutrabirbal@gmail.com>
2019-12-13 11:04:56 +0530

    final

--- Checkbox/async.js
+++ Checkbox/async.js
@@ -8,27 +8,47 @@
     let content = "";
     lines.map((post) => {
     // post.id is unique
-    content += `<input type='checkbox' name="todo-${post.id}" id="todo-${post.id}" />
+    content += `<input type='checkbox' name="${post.id}" id="${post.id}" >
                 <span id="new_${post.id}">${post.title}
                 </span>
+                </input>
             <br>`
-    })
+    });
     document.getElementById("sample").innerHTML = content;
     lines.map((post) => {
-    fetch(`https://jsonplaceholder.typicode.com/todos/todo-${post.id}`, {
-    method: 'PUT',
-    body: JSON.stringify({
-        title: 'Sample',
-        body: 'Instruction',
-    }),
-    headers: {
-        "Content-type": "application/json; charset=UTF-8"
-    }
-    })
-    .then(request => {
-    // If we select a checkbox it strikes through
-        document.getElementById(`todo-${post.id}`).checked = true;
-        document.getElementById(`new_${post.id}`).setAttribute("style" , "text-decoration: line-through;");
-    })
-    })
+        document.getElementById(`${post.id}`).addEventListener("click" , () => {
+            if(document.getElementById(`${post.id}`).checked){
+                fetch(`https://jsonplaceholder.typicode.com/todos/${post.id}`, {
+                method: 'PUT',
+                body: JSON.stringify({
+                    completed: true
+                }),
+                headers: {
+                    "Content-type": "application/json; charset=UTF-8"
+                }
+                })
+                .then(request => {
+                // If we select a checkbox it strikes through
+                    document.getElementById(`new_${post.id}`).setAttribute("style" , "text-decoration: line-through;");
+                })
+            }
+            else
+            {
+                fetch(`https://jsonplaceholder.typicode.com/todos/${post.id}`, {
+                method: 'PUT',
+                body: JSON.stringify({
+                    completed: false
+                    }),
+                headers: {
+                    "Content-type": "application/json; charset=UTF-8"
+                    }
+                })
+                .then(request => {
+                    /*remove strikeout from the post*/
+                    document.getElementById(`new_${post.id}`).setAttribute("style" , "text-decoration: none;");
+                })
+            }
+        });
+    });
 })();
+

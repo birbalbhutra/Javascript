a443c3162bc61888f6b82f73cc91dcf22bf90b53
birbalbhutra <bhutrabirbal@gmail.com>
2019-12-15 20:29:42 +0530

    style

--- posts/script.js
+++ posts/script.js
@@ -1,14 +1,13 @@
 (async function(){
     let request = await fetch('https://jsonplaceholder.typicode.com/posts');
-    // let comments = await fetch('https://jsonplaceholder.typicode.com/comments?postId=1');
     let lines = await request.json();
-    // let comments_lines = await comments.json();
     console.log(lines);
     var i;
-    var list = "";
+    var list = "<div class='firstPg'>";
     for(i=0;i<lines.length;i++){
         list += `<a href="next.html?id=${lines[i].id}" style='text-decoration :none'>${lines[i].title}</a><br>`;
         console.log(lines[i].id);
     }
+    list += "</div>"
     document.getElementById("sample").innerHTML = list;
 })();

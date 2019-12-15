3763ab83da51ce0fa91b7975aa4b5f88720f1500
birbalbhutra <bhutrabirbal@gmail.com>
2019-12-15 19:43:07 +0530

    posts

--- posts/new.js
+++ posts/new.js
@@ -1,6 +1,8 @@
 (async function(){
     let request = await fetch('https://jsonplaceholder.typicode.com/posts');
+    // let comments = await fetch('https://jsonplaceholder.typicode.com/comments?postId=1');
     let lines = await request.json();
+    // let comments_lines = await comments.json();
     console.log(lines);
     var i;
     var list = "";
@@ -8,5 +10,5 @@
         list += `<a href="next.html?id=${lines[i].id}" style='text-decoration :none'>${lines[i].title}</a><br>`;
         console.log(lines[i].id);
     }
-    document.getElementById("sample").innerHTML=list;
+    document.getElementById("sample").innerHTML = list;
 })();
--- posts/new1.js
+++ posts/new1.js
@@ -1,7 +1,25 @@
 (async function(){
     const id = window.location.search.substr(1).split('=')[1];
     console.log(id);
-    let response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
-    let lines = await response.json();
-    document.getElementById("sample").innerHTML = ` UserId: ${lines.userId}<br> Id: ${lines.id}<br> Title: ${lines.title}<br> Body: ${lines.body}<br>`;
-  })();
+    let request = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+    let lines = await request.json();
+    // let comments = await fetch('https://jsonplaceholder.typicode.com/comments?postId=1');
+    // let posts = await comments.json();
+    // if(${posts.postId} === ${lines.id}){
+    //     document.getElementById("sample").innerHTML = `${posts.name}<br> ${posts.email}<br> ${posts.body}<br>`;
+    // }
+    document.getElementById("sample").innerHTML = `<div> UserId: ${lines.userId}<br> Id: ${lines.id}<br> Title: ${lines.title}<br> Body: ${lines.body}<br></div>`;
+    request = await fetch(`https://jsonplaceholder.typicode.com/comments?postId=${id}`);
+    let details = await request.json();
+    // let list = "";
+    // var i;
+    // for(i=0;i<details.length;i++){
+    //   list += `<div>${details.name}<br>${details.email}<br>${details.body}</div>`;
+    // }
+    let list = details.reduce((initial , comment) => {
+      return initial + `<div><h2>${comment.name}<br></h2>${comment.email}<br>
+            <div>${comment.body}</div></div>`;
+    });
+    // list += '</div>';
+    document.getElementById("sample").innerHTML = list;
+})();

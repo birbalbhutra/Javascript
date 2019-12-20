d39db5e1279373687798623b14a40ec6adccafea
birbalbhutra <bhutrabirbal@gmail.com>
2019-12-20 16:27:34 +0530

    new todo fixed

--- Checkbox/async.js
+++ Checkbox/async.js
@@ -11,11 +11,10 @@
     lines.map((post,index) => {
     // post.id is unique
     if(index<=10){
-    content += `<label for="${post.id}"><input type='checkbox' name="${post.id}" id="${post.id}" >
+    content += `<input type='checkbox' name="${post.id}" id="${post.id}" >
                 <span id="new_${post.id}">${post.title}
                 </span>
-                </input>
-            </label><br>`
+                </input><br>`
     }
     });
     // transferring all the lines to div tag in HTML
@@ -64,55 +63,18 @@ function todo_form(form){
     if(form.title.value.length > 0 ){
         var temp = document.getElementById("sample");
         temp.innerHTML +=`<input type='checkbox' name="${form.id.value}" id="${form.id.value}" ><span id="new_${form.id.value}">${form.title.value}</span></input><br>`
-        fetch(`https://jsonplaceholder.typicode.com/todos`, {
+        fetch(`https://jsonplaceholder.typicode.com/todos/${form.id.value}`, {
             method: 'POST',
             body: JSON.stringify({
                 userId: form.userId.value,
                 title: form.title.value,
                 id: form.id.value,
-                completed: true
+                completed: false
             }),
             headers: {
                 "Content-type": "application/json; charset=UTF-8"
             }
         })
-        .then(response => {
-            console.log("fooo");
-            document.getElementById(form.id.value).addEventListener("click" , () => {
-                // If we select a checkbox it strikes through
-                if(document.getElementById(`${form.id.value}`).checked){
-                    fetch(`https://jsonplaceholder.typicode.com/todos/${form.id.value}`, {
-                    method: 'PUT',
-                    body: JSON.stringify({
-                        completed: true
-                    }),
-                    headers: {
-                        "Content-type": "application/json; charset=UTF-8"
-                    }
-                    })
-                    .then(request => {
-                        document.getElementById(`new_${form.id.value}`).setAttribute("style" , "text-decoration: line-through;");
-                    })
-                }
-                else
-                {
-                    fetch(`https://jsonplaceholder.typicode.com/todos/${form.id.value}`, {
-                    method: 'PUT',
-                    body: JSON.stringify({
-                        completed: false
-                        }),
-                    headers: {
-                        "Content-type": "application/json; charset=UTF-8"
-                        }
-                    })
-                    .then(request => {
-                        // If we unselect a checkbox it becomes normal
-                        document.getElementById(`new_${form.id.value}`).setAttribute("style" , "text-decoration: none;");
-                    })
-                }
-            });
-        }
-        )
     }
     else{
         console.log("wrong details");
